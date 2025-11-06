5996166436697e3d7b5c9c3bed9ce07fa8798066
deniseleon <kbenisee7@gmail.com>
2025-11-06 17:20:13 -0500

    create properties, getters, setters, and abstract methods for menu item abstract class

--- src/main/java/com/pluralsight/shop/MenuItem.java
+++ src/main/java/com/pluralsight/shop/MenuItem.java
@@ -1,6 +1,23 @@
 package com.pluralsight.shop;
 
-public class MenuItem {
+public abstract class MenuItem {
     private String name;
 
+    public MenuItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract String getDescription();
+
+    //abstract method to calculate the price
+    public abstract double totalPrice();
 }
+
