07a5e514ca2093692d7f3fa9b6739703a7119ef3
deniseleon <kbenisee7@gmail.com>
2025-11-06 20:49:02 -0500

    Topping class and ToppingType enum class

--- src/main/java/com/pluralsight/shop/Topping.java
+++ src/main/java/com/pluralsight/shop/Topping.java
@@ -1,4 +1,49 @@
 package com.pluralsight.shop;
 
-public class Topping {
+public class Topping extends MenuItem {
+    private ToppingType toppingType;
+
+    public ToppingType getToppingType() {
+        return toppingType;
+    }
+
+    public void setToppingType(ToppingType toppingType) {
+        this.toppingType = toppingType;
+    }
+
+    public Topping(String name, ToppingType toppingType) {
+        super(name);
+        this.toppingType = toppingType;
+    }
+
+    @Override
+    public String getDescription() {
+        String desc = getName() + toppingType;
+
+
+        return desc;
+    }
+
+
+    public double totalPriceForSize(IceCreamSize size) {
+        double price = toppingType.getExtraCharge();
+
+        switch (size) {
+            case CUTTIE_SIZE:
+                return price;
+            case JUST_RIGHT:
+                return price * 1.25;
+            case SIDE_EYE:
+                return price * 1.50;
+            default:
+        //must throw an exception in case size is null
+        throw new IllegalArgumentException("Unknown size: " + size);
+        }
+
+    }
+
+
+    public double totalPrice() {
+        return toppingType.getExtraCharge();
+    }
 }
---
+++ src/main/java/com/pluralsight/shop/ToppingType.java
@@ -0,0 +1,34 @@
+package com.pluralsight.shop;
+
+public enum ToppingType {
+    MYSTICAL("mystical mystery topping", 2.00, true),
+    WEIRD("that doesn't belong on ice cream topping", 1.00, true),
+    FRUIT("fruity toppings", 0.75, true),
+    NUT("nut toppings", 0.00, false),
+    CANDY("candy toppings", 0.00, false),
+    CAKE("cake toppings", 1.00, true),
+    DRIZZLE("drizzle toppings", 0.00, false);
+
+    private final String displayName;
+    private final double extraCharge;
+    private boolean isPremium;
+
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public double getExtraCharge() {
+        return extraCharge;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    ToppingType(String displayName, double extraCharge, boolean isPremium) {
+        this.displayName = displayName;
+        this.extraCharge = extraCharge;
+        this.isPremium = isPremium;
+    }
+}
